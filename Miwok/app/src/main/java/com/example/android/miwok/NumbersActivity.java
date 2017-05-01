616da13e2ab92e6025cded0e40b3ac8f00ad9b8b
Manmohan7 <er.manmohansingh7@gmail.com>
2017-05-01 23:16:44 +0530

    Projects

--- Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -4,6 +4,10 @@ import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -11,13 +15,29 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
-    }
 
-    abstract class NumberListener implements View.OnClickListener
-    {
-        public void OnClick(View view)
+        ArrayList<String> words = new ArrayList<>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        LinearLayout ll = (LinearLayout) findViewById(R.id.rootView);
+
+        int index = 0;
+        while (index < words.size())
         {
+            TextView textView = new TextView(this);
+            textView.setText(words.get(index));
+            ll.addView(textView);
 
+            index++;
         }
     }
 }
